48d5fc4c3c05c99f8c48126720cb95ae569053e4
Christian Oliveira <christianoliveira1908@hotmail.com>
2023-01-21 15:53:01 -0300

    created progress bar component.

---
+++ web/src/components/progress-bar/progress-bar.tsx
@@ -0,0 +1,27 @@
+interface IProgessBarProps {
+    progress: number
+}
+
+const ProgressBar = ({ progress }: IProgessBarProps) => {
+    const progressStyle = {
+        width: `${progress}%`
+    }
+
+    return (
+        <div className='h-3 rounded-xl bg-zinc-700 w-full mt-4'>
+
+
+            <div
+                role='progressbar'
+                aria-label='Progresso de hábitos completados nesse dia'
+                aria-valuenow={progress}
+                className='h-3 rounded-xl bg-violet-600 w-3/4'
+                style={progressStyle}
+            >
+
+            </div>
+        </div>
+    )
+}
+
+export default ProgressBar;
